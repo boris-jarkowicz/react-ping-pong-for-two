07413aab897c095f0e8243d5aa215ab1d6de2f5f
Boris <boris.jarkowicz@jimdo.com>
2017-12-07 03:08:31 +0100

    transform Villain to stateless component

--- src/containers/Villain.js
+++ src/containers/Villain.js
@@ -1,30 +1,17 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
 import PaddleCanvas from '../components/PaddleCanvas';
 
-class Villain extends Component {
-    constructor(props) {
-        super(props);
-
-        console.log('VILLAIN PROPS', this.props);
-    }
-
-    componentDidMount() {
-        console.log('componentDidMount Villain');
-    }
-
-    render() {
-        return (
-            <PaddleCanvas
-                canvasWidth={this.props.canvasWidth}
-                canvasHeight={this.props.canvasHeight}
-                xPos={this.props.xPos}
-                yPos={this.props.yPos}
-                color={this.props.paddleColor}
-            />
-
-        )
-    }
+const Villain = (props) => {
+    return (
+        <PaddleCanvas
+            canvasWidth={props.canvasWidth}
+            canvasHeight={props.canvasHeight}
+            xPos={props.xPos}
+            yPos={props.yPos}
+            color={props.paddleColor}
+        />
+    );
 }
 
 const mapStateToProps = (state) => {
@@ -34,16 +21,7 @@ const mapStateToProps = (state) => {
         ...state.villainProps,
     };
 };
-/*
-const mapDispatchToProps = (dispatch) => {
 
-    return {
-        onKeyPress: (event) => {
-            dispatch(movePaddle(event.keyCode));
-        },
-    }
-};
-*/
 export default connect(
     mapStateToProps
 )(Villain);
